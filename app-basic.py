03b0b01ea2d7c7c41bbc13c8b8dfab229c168e0f
Sagar <sageradh1@gmail.com>
2020-06-26 17:16:34 +0545

    Added body parsing and add resource deocrator

--- app-basic.py
+++ app-basic.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api,reqparse
+from flask_restful import Resource, Api,reqparse,inputs
 
 app = Flask(__name__)
-api = restful.Api(app)
+api = Api(app)
+# api = restful.Api(app)
 
-# parser = reqparse.RequestParser()
-# parser.add_argument('name')
-# parser.add_argument('age', type=int)
-# parser.add_argument('field-with-regex', type=inputs.regex('^[0-9]+$'))
-# args = parser.parse_args()
+parser = reqparse.RequestParser()
 
 class BlankEndpoints(Resource):
     def get(self):
@@ -21,13 +18,13 @@ class EndpointsWithParams(Resource):
     def get(self,params):
         return {'params': params}
 
-# class EndpointWithBody(Resource):
-#     def get(self):
-#         return {'params': params}
-
-# @api.resource('/foo')
-# class EndpointWithInitialAddedRoute(Resource):
-
+class EndpointWithBody(Resource):
+    def get(self):
+        parser.add_argument('name')
+        parser.add_argument('age', type=int)
+        parser.add_argument('field-with-regex', type=inputs.regex('^[0-9]+$'))
+        args = parser.parse_args()
+        return args
 
 #example of endpoint with params
 api.add_resource(EndpointsWithParams,'/<string:params>')
@@ -35,11 +32,15 @@ api.add_resource(EndpointsWithParams,'/<string:params>')
 #all endpoints will be redirected to this resource
 api.add_resource(BlankEndpoints,'/','/nextendpoint','/my-endpoint', endpoint='my_endpoint')
 
-# #endpoints with body
-# api.add_resource(EndpointWithBody,'/','/withbody')
+#endpoints with body
+api.add_resource(EndpointWithBody,'/withbody')
 
-# #endpoints with initial route
-# api.add_resource(EndpointWithInitialAddedRoute)
+# #endpoints with initial string in the route
+@api.resource('/foo')
+class EndpointWithInitialAddedRoute(Resource):
+    def get(self):
+        return "Initial route is foo"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
